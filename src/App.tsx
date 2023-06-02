35fff71565c02f76aafd1499ac993d6fc5b6f8fd
ArtyomKub <coder.kub.lover@gmail.com>
2023-06-02 21:02:46 +0200

    Create a new component 'New component' for learn microtsks 'map'

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import NewComponent from "./NewComponent";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <NewComponent/>
+    );
 }
 
 export default App;
